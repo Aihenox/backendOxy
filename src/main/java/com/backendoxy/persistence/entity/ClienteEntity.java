8b364347cffab09832ed19bf43210ae77ba72113
Lucho <luisfermh212@gmail.com>
2024-06-10 11:05:22 -0500

    Cliente entidad

--- src/main/java/com/backendoxy/persistence/entity/ClienteEntity.java
+++ src/main/java/com/backendoxy/persistence/entity/ClienteEntity.java
@@ -1,30 +1,14 @@
 package com.backendoxy.persistence.entity;
 
-import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name= "clientes")
 @Getter
 @Setter
 @NoArgsConstructor
 public class ClienteEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name= "id_clientes")
-    private Integer idClientes;
-    private String Direccion;
-    private int Telefono;
-    @Email
-    private String Email;
+    
 }
