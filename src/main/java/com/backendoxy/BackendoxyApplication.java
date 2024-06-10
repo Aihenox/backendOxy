baac6357f2491788884b7be33713bebf2198a7f7
Lucho <luisfermh212@gmail.com>
2024-06-09 22:44:39 -0500

    conexion base de datos

---
+++ src/main/java/com/backendoxy/BackendoxyApplication.java
@@ -0,0 +1,13 @@
+package com.backendoxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendoxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendoxyApplication.class, args);
+	}
+
+}
